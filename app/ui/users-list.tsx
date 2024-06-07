4da23ce1b480e36734116b469aceb5debf0ca469
norraVarg <wolf.se2015@gmail.com>
2024-06-07 12:21:16 +0200

    update users page

--- app/ui/users-list.tsx
+++ app/ui/users-list.tsx
@@ -1,5 +1,4 @@
 import { User } from '../lib/definitions'
-import Link from 'next/link'
 
 interface Props {
   ids: number[]
@@ -14,14 +13,12 @@ const UsersList = (props: Props) => {
       <h1 className="flex items-center text-lg font-semibold leading-6 text-gray-900 px-5 sm:hidden">Users</h1>
       <ul role="list" className="row-start-2 row-span-10 divide-y divide-gray-200 overflow-y-auto sm:row-start-1 sm:row-span-11">
         {ids.map(id => (
-          <Link href={`/admin/user-details?id=${id}`} key={id} className="px-5 py-2 flex justify-between hover:bg-sky-100 sm:px-7">
-            <div className="flex flex-col gap-1">
-              <p className="text-xs font-semibold leading-6 text-gray-900">{entities[id].name}</p>
-              <p className="truncate text-xs leading-5 text-gray-500">Company: {entities[id].company.name}</p>
-              <p className="truncate text-xs leading-5 text-gray-500">Email: {entities[id].email}</p>
-              <p className="truncate text-xs leading-5 text-gray-500">Phone: {entities[id].phone}</p>
-            </div>
-          </Link>
+          <div className="flex flex-col gap-1 px-5 py-2 sm:px-7">
+            <p className="text-xs font-semibold leading-6 text-gray-900">{entities[id].name}</p>
+            <p className="truncate text-xs leading-5 text-gray-500">Website: {entities[id].website}</p>
+            <p className="truncate text-xs leading-5 text-gray-500">Email: {entities[id].email}</p>
+            <p className="truncate text-xs leading-5 text-gray-500">Phone: {entities[id].phone}</p>
+          </div>
         ))}
       </ul>
     </main>
